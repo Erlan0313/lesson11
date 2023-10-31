c5c4691a68f0eb21ac0334d916dd70229e583176
Erlan0313 <attokuroverlan2003@gmail.com>
2023-11-01 00:58:46 +0600

    first commit

--- lesson11.dart
+++ lesson11.dart
@@ -1,38 +1,84 @@
 import 'dart:io';
+import 'dart:math';
 
 void main(List<String> args) {
-// //   Необходимо написать программу, которая угадывает загаданное пользователем число от 1 до 100,
-// // за как можно меньшее количество шагов. Программа выдаёт предполагаемые числа, а пользователь отвечает, больше его число или меньше, чем число на экране.
-// // Допустим, пользователь загадал число 32, тогда вывод программы может быть похож на этот:
-binarySearch(65);
+  int randomNum = Random().nextInt(5);
+  print(randomNum);
+  int min = 50;
+  int max = 100;
+  int randomNum2 = min + Random().nextInt(max - min);
+  print(randomNum2);
+  task2();
+  task3();
+  task4();
 }
 
-void binarySearch(int a) {
-  int min = 1;
-  int max = 100;
-  int schet = 0;
-  int middle = (min + max) ~/ 2;
-  print('Инструкция если больше g иначе l');
-  stdout.write('Придумай число я угадаю: ');
-  int number = int.parse(stdin.readLineSync()!);
-  while (true) {
-    if (number.runtimeType == int && number > 0 && number <= 100) {
-      schet++;
-      stdout.write('Is it $middle: ');
-      String user = stdin.readLineSync()!;
-      if (user == 'l') {
-        max = middle - 1;
-      } else if (user == 'g') {
-        min = middle + 1;
-      }
-      if (user == 'yes') {
-        print('Число которой вы придумали: $number');
-        print('Я угадал на $schet попытке');
-      }
-    } else {
-      print('Ошибка! Провертьте ввели ли вы число и является ли оно < 100');
+// 1) Выведите на экран сумму двух случайно сгенерированных чисел
+task1() {
+  int randomNum1 = Random().nextInt(10);
+  int randomNum2 = Random().nextInt(10);
+  int sum = randomNum1 + randomNum2;
+  print(randomNum1);
+  print(randomNum2);
+  print(sum);
+}
+
+// 2) Заполните массив случайными числами, размер массива задайте через
+// рандом
+task2() {
+  List<int> array = [];
+  int len = Random().nextInt(20);
+  for (int i = 0; i < len; i++) {
+    array.add(Random().nextInt(10));
+  }
+  print(array);
+}
+
+// 3)Заполните массив случайными числами в интервале [0,5]. Введите число X
+// и найдите все индексы значения, равные X.
+task3() {
+  List<int> array = [];
+  int len = 10;
+  for (int i = 0; i < len; i++) {
+    array.add(Random().nextInt(10));
+  }
+  print(array);
+  stdout.write('Введите число Х: ');
+  int x = int.parse(stdin.readLineSync()!);
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    if (x == array[i]) {
+      print('Индекс схожим с числом Х: $count');
+    } else if (count == 0) {
+      print('v massive net takogo chisla');
     }
-    middle = (min + max) ~/ 2;
+    count++;
   }
 }
 
+// 4)Заполните массив случайными числами в интервале [50,100]. Определить,
+// есть ли в нем элементы с одинаковыми значениями, стоящие рядом.
+task4() {
+  int min = 50;
+  int max = 100;
+  int randomNum2 = min + Random().nextInt(max - min);
+  List<int> array = [];
+  int len = 10;
+  for (int i = 0; i < len; i++) {
+    array.add(min + Random().nextInt(max - min));
+  }
+  print(array);
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    if (array[i] == array[i + 1]) {
+      count++;
+      break;
+    }
+  }
+  if (count == 1) {
+    print('yes');
+  } else {
+    print('no');
+  }
+
+}
